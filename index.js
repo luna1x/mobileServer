58fd9bc4d878a85a0ced5ad43b39890e3874a75e
luna1x <luna1x@gmail.com>
2013-07-03 22:26:48 +0900

    modify redis setting

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var socketHandlers = require('./socketHandlers');
 var redis = require('redis');
 var url = require('url');
 
-process.env.REDISCLOUD_URL = "redis://rediscloud:hUASkTEjmAlbh6De@pub-redis-18793.us-east-1-2.1.ec2.garantiadata.com:18793";
+// process.env.REDISCLOUD_URL = "redis://rediscloud:hUASkTEjmAlbh6De@pub-redis-18793.us-east-1-2.1.ec2.garantiadata.com:18793";
 
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-redisClient.auth(redisURL.auth.split(":")[1]);
+// var redisURL = url.parse(process.env.REDISCLOUD_URL);
+// var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+// redisClient.auth(redisURL.auth.split(":")[1]);
+
+var redisClient = redis.createClient(6379, "54.250.134.172", {no_ready_check: true});
 
 server.start(requestHandlers.handle, socketHandlers.handle, redisClient);
