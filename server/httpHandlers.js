9510c714f5c066fb430682665c8c9b514b312778
luna1x <luna1x@gmail.com>
2013-07-17 01:29:12 +0900

    add /ping

--- server/httpHandlers.js
+++ server/httpHandlers.js
@@ -8,10 +8,16 @@ function handle(app) {
 	app.get('/reward', reward.reward);
 
 	app.get('faviconIco', faviconIco);
+	app.get('/ping', ping);
 }
 
 
 function faviconIco() {
+}
+
+function ping(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/html'});
+	response.end('pong');
 }
 
 exports.handle = handle;
